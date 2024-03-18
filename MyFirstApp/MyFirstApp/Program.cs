d082891b50b6ab345937c636fd0bccf171de223a
s27219 <s27219@pjwstk.edu.pl>
2024-03-18 15:48:05 +0100

    add method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double GetAverage(int[] numbers)
+{
+    var suma = 0;
+    foreach (var number in numbers)
+    {
+        suma += number;
+    }
+
+    return (double) suma / numbers.Length;
+}
